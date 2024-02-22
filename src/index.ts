6f09c806e596b9b27a51bbfaef55fbe7e4cd7635
Jussi Kinnula <jussi.kinnula@wunderdog.fi>
2024-02-22 20:23:42 +0200

    Added more texts and fixed loop firing mechanism

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ scene.add(light);
 const light2 = new THREE.PointLight(0x0000ff, 2, 50);
 light2.power = 100;
 scene.add(light2);
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.001, 100);
+const camera = new THREE.PerspectiveCamera(45, ASPECT_X / ASPECT_Y, 0.001, 100);
 camera.position.z = 400;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(PX_WIDTH, PX_HEIGHT);
@@ -48,7 +48,7 @@ function render() {
 function resize() {
   let width = window.innerWidth;
   let height = window.innerHeight;
-  if (width / height >= ASPECT_X / ASPECT_Y) {
+  if (width / height > ASPECT_X / ASPECT_Y) {
     width = window.innerHeight / ASPECT_Y * ASPECT_X;
     const margin = (window.innerWidth - width) / 2;
     renderer.domElement.style.marginLeft = `${margin}px`;
@@ -62,6 +62,14 @@ function resize() {
   renderer.domElement.style.width = `${width}px`;
   renderer.domElement.style.height = `${height}px`;
   camera.updateProjectionMatrix();
+}
+
+const fired: Record<string, boolean> = {};
+function fireOnce(id: string, callback: Function) {
+  if (!fired[id]) {
+    fired[id] = true;
+    callback();
+  }
 }
 
 (async function() {
@@ -89,6 +97,7 @@ function resize() {
     resize();
     if (!sound.isPlaying) {
       sound.play();
+      requestAnimationFrame(render);
     }
   });
 
@@ -100,20 +109,30 @@ function resize() {
   };
 
   resize();
-  requestAnimationFrame(render);
   window.addEventListener('resize', resize);
 
   const fonts = await loadFont();
 
   const text1 = getText(fonts[0], "Puoluehallitus\n       presents...", path.getPointAt(0.08 % 1), { x: 4, y: -1.5 });
-  scene.add(text1);
-  text1.setRotationFromQuaternion(camera.quaternion);
 
   const text2 = getText(fonts[0], "PURRA\n    ORPOA", path.getPointAt(0.20 % 1), { z: -1 });
   scene.add(text2);
 
   const text3 = getText(fonts[0], "Music by:\n    Puoluehallitus\n\nCode by: Spot", path.getPointAt(0.30 % 1), { z: -3 });
   scene.add(text3);
+
+  const text4 = getText(fonts[0], "Greetings\n      to...", path.getPointAt(0.50 % 1), { x: -4 });
+  scene.add(text4);
+  const text5 = getText(fonts[0], "Jml", path.getPointAt(0.51 % 1), { x: -4 });
+  scene.add(text5);
+  const text6 = getText(fonts[0], "Accession", path.getPointAt(0.52 % 1), { x: -4 });
+  scene.add(text6);
+  const text7 = getText(fonts[0], "Byterapers", path.getPointAt(0.53 % 1), { x: -4 });
+  scene.add(text7);
+  const text8 = getText(fonts[0], "...especially\n     Nyyrikki", path.getPointAt(0.54 % 1), { x: -4 });
+  scene.add(text8);
+  const text9 = getText(fonts[0], "Trsac", path.getPointAt(0.55 % 1), { x: -4 });
+  scene.add(text9);
 
   const ending = new THREE.SpriteMaterial({
     map: textures[1],
@@ -133,15 +152,37 @@ function resize() {
     percentage = time / maxTime;
     const roundedPercentage = Math.round(percentage * 1000) / 10;
 
+    if (roundedPercentage === 0.1) {
+      fireOnce('text1', () => {
+        scene.add(text1);
+        text1.setRotationFromQuaternion(camera.quaternion);
+      });
+    }
+
     if (roundedPercentage === 11.5) {
-      text2.setRotationFromQuaternion(camera.quaternion);
-      text3.setRotationFromQuaternion(camera.quaternion);
+      fireOnce('text2_3', () => {
+        text2.setRotationFromQuaternion(camera.quaternion);
+        text3.setRotationFromQuaternion(camera.quaternion);
+      });
+    }
+
+    if (roundedPercentage === 44) {
+      fireOnce('creditsTexts', () => {
+        text4.setRotationFromQuaternion(camera.quaternion);
+        text5.setRotationFromQuaternion(camera.quaternion);
+        text6.setRotationFromQuaternion(camera.quaternion);
+        text7.setRotationFromQuaternion(camera.quaternion);
+        text8.setRotationFromQuaternion(camera.quaternion);
+        text9.setRotationFromQuaternion(camera.quaternion);
+      });
     }
 
     if (roundedPercentage === 88) {
-      const p = path.getPointAt((percentage + 0.024) % 1)
-      sprite.position.set(p.x, p.y, p.z);
-      scene.add(sprite);
+      fireOnce('endCredits', () => {
+        const p = path.getPointAt((percentage + 0.024) % 1)
+        sprite.position.set(p.x, p.y, p.z);
+        scene.add(sprite);
+      });
     }
 
     if (percentage > 0.9 && percentage <= 0.97) {
